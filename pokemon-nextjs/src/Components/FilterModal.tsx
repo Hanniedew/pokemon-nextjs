36ee93be4747c65cd23a99c9dac9ff55aa183b70
lihan <wong.lihan48@gmail.com>
2024-05-11 00:43:51 +0800

    added a working filter component

--- pokemon-nextjs/src/Components/FilterModal.tsx
+++ pokemon-nextjs/src/Components/FilterModal.tsx
@@ -1,36 +1,165 @@
-"use client";
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
-import React, { useState } from "react";
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+
+import { useState } from 'react';
 
 function FilterModal() {
-  //   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const openModal = () => {
-    setIsModalOpen(!isModalOpen);
+  const [type1, setType1] = useState('');
+
+  const handleType1Change = (event: SelectChangeEvent) => {
+    setType1(event.target.value);
   };
 
+  const [type2, setType2] = useState('');
+
+  const handleType2Change = (event: SelectChangeEvent) => {
+    setType2(event.target.value);
+  };
+
+  const [generation, setGeneration] = useState('');
+
+  const handleGenChange = (event: SelectChangeEvent) => {
+    setGeneration(event.target.value);
+  };
   return (
-    <>
-      {isModalOpen && (
-        // <div className="relative">
-        //   <div className="absolute top-full left-0 mt-2 w-full bg-white border border-gray-300 shadow-md rounded-lg p-4">
-        //     {/* Modal Content */}
-        //     <div className="text-lg font-bold mb-4">Filter Pokémon</div>
-        //     <div className="flex flex-col">
-        //       <label className="text-gray-700 mb-2">Enter Pokémon name:</label>
-        //       <input
-        //         id="pokemonName"
-        //         type="text"
-        //         className="w-full px-6 py-2 bg-gray-200 hover:bg-gray-300 transition-colors duration-300 rounded-full focus:outline-none"
-        //         placeholder="Pokémon name"
-        //       />
-        //     </div>
-        //   </div>
-        // </div>
-        <div>tes</div>
-      )}
-    </>
+   
+    <div className="relative">
+      <div className="mt-10">
+        
+        <div className="text-lg font-bold mb-6 text-center">Filter Pokémon</div>
+        {/* Container to hold filter criterion */}
+
+        {/* Pokémon name */}
+        <div className="flex flex-row"> 
+          <div className="flex flex-col mr-4">
+            <label className="text-gray-700 mb-2 ml-3">Pokémon name:</label>
+            <input
+              id="pokemonName"
+              type="text"
+              className="w-full px-4 py-2 bg-gray-200 hover:bg-gray-300 transition-colors duration-300 rounded-full focus:outline-none"
+              placeholder="Enter Pokémon name"
+            />
+          </div>
+
+        {/* Type 1 */}
+          <div className="flex flex-col mr-4">
+            <label className="text-gray-700 ml-3">Type 1:</label>
+            <FormControl sx={{ m: 1, minWidth: 120 }}>
+              <Select
+                value={type1}
+                onChange={handleType1Change}
+                displayEmpty
+                sx={{ height: '40px', width: '150px' }}
+              >
+                <MenuItem value="">
+                  <em>Please select</em>
+                </MenuItem>
+                <MenuItem value={1}>Ice</MenuItem>
+                <MenuItem value={2}>Water</MenuItem>
+                <MenuItem value={3}>Grass</MenuItem>
+                <MenuItem value={4}>Dragon</MenuItem>
+                <MenuItem value={5}>Fairy</MenuItem>
+                <MenuItem value={6}>Electric</MenuItem>
+                <MenuItem value={7}>Normal</MenuItem>
+                <MenuItem value={8}>Physic</MenuItem>
+                <MenuItem value={9}>Bug</MenuItem>
+                <MenuItem value={10}>Fire</MenuItem>
+                <MenuItem value={11}>Poison</MenuItem>
+                <MenuItem value={12}>Steel</MenuItem>
+                <MenuItem value={13}>Fighting</MenuItem>
+                <MenuItem value={14}>Flying</MenuItem>
+                <MenuItem value={15}>Dark</MenuItem>
+                <MenuItem value={16}>Ground</MenuItem>
+                <MenuItem value={17}>Ghost</MenuItem>
+                <MenuItem value={18}>Rock</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+
+          {/* Type 2 */}
+          <div className="flex flex-col mr-4">
+            <label className="text-gray-700 ml-3">Type 2:</label>
+            <FormControl sx={{ m: 1, minWidth: 120 }}>
+              <Select
+                value={type2}
+                onChange={handleType2Change}
+                displayEmpty
+                sx={{ height: '40px', width: '150px' }}
+              >
+                <MenuItem value="">
+                  <em>Please select</em>
+                </MenuItem>
+                <MenuItem value={1}>Ice</MenuItem>
+                <MenuItem value={2}>Water</MenuItem>
+                <MenuItem value={3}>Grass</MenuItem>
+                <MenuItem value={4}>Dragon</MenuItem>
+                <MenuItem value={5}>Fairy</MenuItem>
+                <MenuItem value={6}>Electric</MenuItem>
+                <MenuItem value={7}>Normal</MenuItem>
+                <MenuItem value={8}>Physic</MenuItem>
+                <MenuItem value={9}>Bug</MenuItem>
+                <MenuItem value={10}>Fire</MenuItem>
+                <MenuItem value={11}>Poison</MenuItem>
+                <MenuItem value={12}>Steel</MenuItem>
+                <MenuItem value={13}>Fighting</MenuItem>
+                <MenuItem value={14}>Flying</MenuItem>
+                <MenuItem value={15}>Dark</MenuItem>
+                <MenuItem value={16}>Ground</MenuItem>
+                <MenuItem value={17}>Ghost</MenuItem>
+                <MenuItem value={18}>Rock</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          
+          {/* Generation */}
+          <div className="flex flex-col mr-6">
+            <label className="text-gray-700 ml-3">Generation:</label>
+            <FormControl sx={{ m: 1, minWidth: 120 }}>
+              <Select
+                value={generation}
+                onChange={handleGenChange}
+                displayEmpty
+                sx={{ height: '40px', width: '150px' }}
+              >
+                <MenuItem value="">
+                  <em>Please select</em>
+                </MenuItem>
+                <MenuItem value={1}>1</MenuItem>
+                <MenuItem value={2}>2</MenuItem>
+                <MenuItem value={3}>3</MenuItem>
+                <MenuItem value={4}>4</MenuItem>
+                <MenuItem value={5}>5</MenuItem>
+                <MenuItem value={6}>6</MenuItem>
+                
+              </Select>
+            </FormControl>
+          </div>
+
+          {/* Legendary */}
+          <FormControl>
+            <FormLabel id="demo-radio-buttons-group-label">Legendary</FormLabel>
+            <RadioGroup
+              aria-labelledby="legendary-radio-buttons-group-label"
+              name="legendary-radio-buttons-group"
+            >
+              <FormControlLabel value="true" control={<Radio />} label="True" />
+              <FormControlLabel value="false" control={<Radio />} label="False" />
+            </RadioGroup>
+          </FormControl>
+        
+        </div>
+        
+      </div>
+    </div>
+     
+     
   );
 }
 export default FilterModal;
--- pokemon-nextjs/src/Components/SearchBar.tsx
+++ pokemon-nextjs/src/Components/SearchBar.tsx
@@ -3,10 +3,13 @@ import Image from "next/image";
 import FilterIcon from "../Icons/filter.svg";
 import SearchIcon from "../Icons/search.svg";
 import FilterModal from "./FilterModal";
+import { useState } from "react";
 
 function SearchBar() {
+  const [isFilterClicked, setIsFilterClicked] = useState(false);
   const handleFilter = () => {
     console.log("Filter button clicked");
+    setIsFilterClicked(!isFilterClicked);
   };
 
   const handleSearch = () => {
@@ -15,45 +18,51 @@ function SearchBar() {
 
   return (
     <div className="flex justify-center">
-      <div className="relative w-4/5 md:w-3/5 mt-8 flex items-center">
-        {/* Search Input */}
-        <input
-          type="text"
-          className="w-full text-lg h-12 px-8 py-3 pr-36 border border-gray-500 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-          placeholder="Search..."
-        />
-        {/* Vertical Pipe */}
-        <div className="absolute right-28 sm:right-32 h-full flex items-center">
-          <div className="h-full border-l border-gray-500"></div>
-        </div>
+      {/* Container for the whole search UI */}
+      <div className="flex flex-col w-4/5 items-center">
+        {/* Search portion */}
+        <div className="relative w-full md:w-3/5 mt-8 flex ">
+          {/* Search Input */}
+          <input
+            type="text"
+            className="w-full text-lg h-12 px-8 py-3 pr-36 border border-gray-500 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+            placeholder="Search..."
+          />
+          {/* Vertical Pipe */}
+          <div className="absolute right-28 sm:right-32 h-full flex items-center">
+            <div className="h-full border-l border-gray-500"></div>
+          </div>
 
-        {/* First Icon */}
-        <div className="absolute right-16 h-full flex items-center mr-2  cursor-pointer">
-          <div className="flex items-center">
-            {/* <FilterModal /> */}
-            <button onClick={handleFilter}>
-              <Image
-                src={FilterIcon}
-                width={25}
-                height={25}
-                alt="Filter Icon"
-              />
-            </button>
+          {/* First Icon */}
+          <div className="absolute right-16 h-full flex items-center mr-2  cursor-pointer">
+            <div className="flex items-center">
+              {/* <FilterModal /> */}
+              <button onClick={handleFilter}>
+                <Image
+                  src={FilterIcon}
+                  width={25}
+                  height={25}
+                  alt="Filter Icon"
+                />
+              </button>
+            </div>
+          </div>
+          {/* Second Icon */}
+          <div className="absolute right-8 h-full flex items-center  cursor-pointer">
+            <div className="flex items-center">
+              <button onClick={handleSearch}>
+                <Image
+                  src={SearchIcon}
+                  width={25}
+                  height={25}
+                  alt="Search Icon"
+                />
+              </button>
+            </div>
           </div>
         </div>
-        {/* Second Icon */}
-        <div className="absolute right-8 h-full flex items-center  cursor-pointer">
-          <div className="flex items-center">
-            <button onClick={handleSearch}>
-              <Image
-                src={SearchIcon}
-                width={25}
-                height={25}
-                alt="Search Icon"
-              />
-            </button>
-          </div>
-        </div>
+        {/* Filter Modal */}
+        {isFilterClicked && <FilterModal />}
       </div>
     </div>
   );
