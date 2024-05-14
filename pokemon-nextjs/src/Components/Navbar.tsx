17eae7dbb3d92e55f1ad0a0950fbdcd5a36204f3
HannieDew <wong.lihan48@gmail.com>
2024-05-14 10:44:20 +0800

    added a home icon to route back to home page

--- pokemon-nextjs/src/Components/Navbar.tsx
+++ pokemon-nextjs/src/Components/Navbar.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
+import HomeIcon from "../Icons/home.svg";
+import Link from "next/link";
+
 function Navbar() {
   return (
     <div className="h-28 flex justify-center items-center border-b-4 border-neutral-700 bg-neutral-200">
       <h1 className="text-4xl">Pokédex</h1>
-    </div>  )
+      <div className="absolute right-4 top-20 cursor-pointer">
+        <Link href={`/`}>
+          <Image src={HomeIcon} width={25} height={25} alt="Home Icon" />
+        </Link>{" "}
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
