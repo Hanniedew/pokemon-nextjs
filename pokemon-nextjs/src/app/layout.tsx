c389c94d396e879b8dd995a135dfb96d5d694b04
HannieDew <wong.lihan48@gmail.com>
2024-05-20 15:09:14 +0800

    changed the webpage icon

--- pokemon-nextjs/src/app/layout.tsx
+++ pokemon-nextjs/src/app/layout.tsx
@@ -9,6 +9,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Pokédex",
   description: "Pokédex created by next.js",
+  icons: {
+    icon: "/icon.ico",
+  },
 };
 
 export default function RootLayout({
